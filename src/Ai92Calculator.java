e870687e961403d9171c618cfe1445196eb78c72
YanJulia <tmip.poliakova@yandex.ru>
2023-01-29 08:33:38 +0300

    last

--- src/Ai92Calculator.java
+++ src/Ai92Calculator.java
@@ -1,7 +1,7 @@
 public class Ai92Calculator {
-    double price = 42.74;
+    double ai92price = 42.74;
     double tax = 0.13;
-    double fullPrice = price * (1 + tax);
+    double fullPrice = ai92price * (1 + tax);
 
     public double calculate(double volume) {
         return volume * fullPrice;
